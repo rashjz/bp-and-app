4d0bf456468a46a6bfa846537e507a07a8e7bb23
Rashad <rashjz@users.noreply.github.com>
2016-09-20 10:55:57 +0400

    Update SelectedItemActivity.java

--- app/src/main/java/rashjz/info/bakuposter/com/activity/SelectedItemActivity.java
+++ app/src/main/java/rashjz/info/bakuposter/com/activity/SelectedItemActivity.java
@@ -265,7 +265,7 @@ public class SelectedItemActivity extends AppCompatActivity {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("Authorization", "cmFzaGp6OnBhcmtldDQ3MA==");
+                params.put("Authorization", "xxxxxxxxxxxx==");
                 return params;
             }
         };
